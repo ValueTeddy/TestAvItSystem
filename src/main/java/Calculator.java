39a81e0a3fda52da085c1e73b04a8adac26ac0a5
Teddy <teodor.adolfsson@gmail.com>
2022-04-01 16:34:42 +0200

    write tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,21 @@
-public class Calculator {
-    public int add(int a, int b) {
-        return (a + b);
+import java.util.Objects;
 
+
+public class Calculator {
+    public static int adder(int a, int b) {
+        return a+b;
     }
-}
+
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+
+    public static boolean isTrue(String a, String b) {
+        return Objects.equals(a, b);
+    }
+
+    public static double pow(double x, double y) {
+        return Math.pow(x,y);
+    }
+    }
+
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,76 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Timeout(1) // Cause tests to fail if they are too slow
 class CalculatorTest {
-    @Test
-    @DisplayName("2 + 1 = 3")
-    void addsTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(3, calculator.add(2,1), "2 + 1 should equal 3");
+
+    @BeforeAll // runs once before the entire test suite
+    static void beforeAll() {
+        System.out.println("this is printed before all tests are run");
     }
+
+    @BeforeEach // runs before each test
+    void beforeEach(TestInfo info) {
+        // the test info class can get useful info from each test
+        System.out.println("running " + info.getDisplayName());
+    }
+
+    @AfterEach // runs once after each test
+    @Disabled // skips running the test but keeps it in logging and test coverage
+    void afterEach() {
+        System.out.println("this is printed after each test is ran");
+    }
+
+    @Test
+    @DisplayName("addition")
+    void testAdder() {
+        // this should pass
+        assertEquals(3, Calculator.adder(1,2), "1 + 2 = 3");
+    }
+
+    @Test
+    @DisplayName("subtract")
+    void testSubtract() {
+        // this should pass
+        assertEquals(1, Calculator.subtract(3,2),"3 - 2 = 1");
+    }
+
+    @Test
+    @Disabled // this one is disabled
+    @DisplayName("slowTest")
+    void slowTest() throws InterruptedException {
+        // this is a slow test
+        TimeUnit.SECONDS.sleep(2);
+        assertEquals(1, Calculator.subtract(3,2),"this is a slow test");
+    }
+
+    @Test
+    @DisplayName("test that intentionally fails")
+    void isTrue(){
+        // this should fail
+        assertTrue(Calculator.isTrue("hello", "World"), "string a == string b");
+    }
+
+    @Test
+    @DisplayName("test x^y")
+    void powTest() {
+        long expected = 4294967296L;
+        int x = 4;
+        int y = 16;
+        assertEquals(expected, Calculator.pow(x,y), "x raised to the power of y");
+    }
+
+    @Test
+    @DisplayName("test array len and content")
+    void arrayLen() {
+        int arrLen = 3;
+        String[] arr = {"Foo ","Bar ","World"};
+        String[] arrExpected = {"Foo ","Bar ","World"};
+        assertEquals(arrLen, arr.length);
+        assertArrayEquals(arrExpected, arr);
+    }
+
 }
